e90537dbd69afa4d847ac2e8ced5c1f0142e03f5
gtohkoots <kt9sh@virginia.edu>
2021-11-29 21:36:53 -0500

    my post

---
+++ editPost.php
@@ -0,0 +1,54 @@
+<html>
+<?php
+include("common-head.php"); 
+include("mysql-helper.php");
+?>
+<head>
+
+<meta name="author" content="Ketian Tu">
+<meta name="description" content="My Post">
+
+<title>My Post</title>
+<link rel="stylesheet" href="main.css" />
+<link rel="stylesheet" href="editPost.css" />
+</head>
+<body>
+<?php include('header.php') ?>
+<?php 
+    $pid = $_GET['pid'];
+    $sql = "SELECT fname, lname, name, details, time, capacity FROM user NATURAL JOIN topic NATURAL JOIN post WHERE pid = ?";
+    $result = execute_query($sql,array($pid))['rows_affected'];
+?>
+<div class="container-fluid app-wrapper">
+    <div class="white-bar"></div>
+    <p class="display-4" style="color:black;">You’re viewing a post of yours</p>
+    <div class="content-wrapper">
+        <div class="list-group">
+            <li class="list-group-item">
+                <h3>Topic: <?php echo $result[0]["name"] ?></h3>
+            </li>
+            <li class="list-group-item">
+                <h3>Time: <?php echo $result[0]["time"] ?></h3>
+            </li>
+            <li class="list-group-item">
+                <h3>Capacity: <?php echo $result[0]["capacity"] ?></h3>
+                <button type="submit" class="btn btn-lg">Reserve</button>
+            </li>
+            <li class="list-group-item">
+                <h3>Details: <?php echo $result[0]["details"] ?></h3>
+            </li>
+            <?php 
+                if(!empty($post_err)){
+                    echo '<div class="alert alert-danger">' . $post_err . '</div>';
+                }        
+                if(!empty($post_suc)){
+                    echo '<div class="alert alert-success">' . $post_suc . '</div>';
+                    sleep(3);
+                    header("location: mainpage.php");
+                }        
+            ?>
+        </div>
+    </div>
+</div>
+</body>
+</html>
--- myPost.php
+++ myPost.php
@@ -6,13 +6,41 @@ include("mysql-helper.php");
 <head>
 
 <meta name="author" content="Ketian Tu">
-<meta name="description" content="Main Page">
+<meta name="description" content="My Post">
 
-<title>Login</title>
+<title>My Post</title>
 <link rel="stylesheet" href="main.css" />
+<link rel="stylesheet" href="mainpage.css" />
 </head>
 <body>
 <?php include('header.php') ?>
+<?php 
+    $cid = "";
+    $posts = array();
+    if(isset($_SESSION['cid'])){
+        $cid = $_SESSION['cid'];
+        // retreieve posts from other users
+        $sql = "SELECT pid, cid, time, details, name FROM post NATURAL JOIN topic WHERE cid = ? ";
+        $retrieve_result = execute_query($sql,array($cid));
+        $posts = $retrieve_result['rows_affected'];
+    }
+?>
+<div class="container-fluid app-wrapper">
+    <div class="white-bar"></div>
+    <p class="display-4" style="color:black;">You're viewing my posts</p>
+    <div class="content-wrapper">
+        <div class="list-group">
+            <?php for($i = 0; $i < count($posts); $i++): ?>
+                <a href="editPost.php?pid=<?php echo $posts[$i]['pid'] ?>">
+                    <li class="post-item">
+                        <span><?php echo $posts[$i]['details'] ?></span>
+                        <span>Topic: <?php echo $posts[$i]['name'] ?></span>
+                    </li>
+                </a>
+            <?php endfor; ?>
+        </div>
+    </div>
+</div>
 </body>
 
 </html>
